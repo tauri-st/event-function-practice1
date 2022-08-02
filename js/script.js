31664187d86b47061d9ee93adf0c55bd028d7931
Tauri StClaire <104536950+tauri-st@users.noreply.github.com>
2022-08-01 17:53:09 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var box1 = document.querySelector(".box-1");
+var box2 = document.querySelector(".box-2");
+var box3 = document.querySelector(".box-3");
+
+document.addEventListener("keydown", function (e) {
+  var key = e.key;
+  if (key === "1") {
+    box1.innerText = "🦄";
+  } else if (key === "2") {
+    box2.innerText = "🐧";
+  } else if (key === "3") {
+    box3.innerText = "🦚";
+  }
+});
+
+var button = document.querySelector("button");
+button.addEventListener("click", function () {
+  box1.innerText = "1";
+  box2.innerText = "2";
+  box3.innerText = "3";
+});
